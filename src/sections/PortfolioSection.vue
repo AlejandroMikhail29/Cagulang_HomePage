a978028079217e989b3904a0fe9d57aaa6d336da
AlejandroMikhail29 <114491695+AlejandroMikhail29@users.noreply.github.com>
2023-03-12 22:46:51 +0800

    Add files via upload

---
+++ src/sections/PortfolioSection.vue
@@ -0,0 +1,81 @@
+<template>
+    <!-- Portfolio section start -->
+	<section id="portfolio-section" class="px-4 lg:px-8 py-16  relative">
+        <!-- Text wrapper start -->
+        <div class="flex gap-8 flex-col md:flex-row">
+            <!-- Main text start -->
+            <h1 class="uppercase text-5xl mb-4 font-semibold ">
+                OUR PORTFOLIO
+            </h1>
+            <p class="capitalize xl:w-1/2 text-stone-500 mb-8">
+              historical sites that you will appreciate more if you see personally within your eyes.
+            </p>
+            <!-- Main text end -->
+        </div>
+        <!-- Text wrapper end -->
+
+        <!-- Images grid start -->
+		<div class="grid-cols-2 md:grid-cols-5 grid mb-8 gap-4">
+            <div class="grid grid-cols-1 md:hidden gap-4">
+                <img src="@/assets/portfolio/p1.jpg" alt="1" class="rounded-xl">
+                <img src="@/assets/portfolio/p2.jpg" alt="2" class="rounded-xl">
+                <img src="@/assets/portfolio/p3.jpg" alt="3" class="rounded-xl">
+                <img src="@/assets/portfolio/p4.jpg" alt="4" class="rounded-xl">
+                <img src="@/assets/portfolio/p5.jpg" alt="5" class="rounded-xl">
+            </div>
+            <div class="grid grid-cols-1 md:hidden gap-4">
+                <img src="@/assets/portfolio/p6.jpg" alt="6" class="rounded-xl">
+                <img src="@/assets/portfolio/p7.jpg" alt="7" class="rounded-xl">
+                <img src="@/assets/portfolio/p8.jpg" alt="8" class="rounded-xl">
+                <img src="@/assets/portfolio/p9.jpg" alt="9" class="rounded-xl">
+                <img src="@/assets/portfolio/p10.jpg" alt="10" class="rounded-xl">
+            </div>
+
+            <div class="hidden md:flex flex-col gap-4">
+                <img src="@/assets/portfolio/p1.jpg" alt="1" class="rounded-xl">
+                <img src="@/assets/portfolio/p2.jpg" alt="2" class="rounded-xl">
+            </div>
+            <div class="hidden md:flex flex-col gap-4">
+                <img src="@/assets/portfolio/p3.jpg" alt="1" class="rounded-xl">
+                <img src="@/assets/portfolio/p4.jpg" alt="2" class="rounded-xl">
+            </div>
+            <div class="hidden md:flex flex-col gap-4">
+                <img src="@/assets/portfolio/p8.jpg" alt="2" class="rounded-xl">
+                <img src="@/assets/portfolio/p5.jpg" alt="2" class="rounded-xl">
+            </div>
+            <div class="hidden md:flex flex-col gap-4">
+                <img src="@/assets/portfolio/p7.jpg" alt="1" class="rounded-xl">
+                <img src="@/assets/portfolio/p6.jpg" alt="2" class="rounded-xl">
+            </div>
+            <div class="hidden md:flex flex-col gap-4">
+                <img src="@/assets/portfolio/p9.jpg" alt="1" class="rounded-xl">
+                <img src="@/assets/portfolio/p10.jpg" alt="2" class="rounded-xl">
+            </div>
+        </div>
+		<!-- Images grid end -->
+
+        <!-- "More" link start -->
+        <a href="#" class="text-end">
+            <p class="font-semibold text-lg group relative">
+                <span>Show more photos </span>
+                <i class="fa-solid fa-arrow-right"></i>
+            </p>
+        </a>
+        <!-- "More" link end -->
+
+        <!-- Circle start -->
+        <div class="bg-neutral-300 h-64 w-64 rounded-full absolute top-0 left-0 mt-16 -z-20"></div>
+        <!-- Circle end -->
+	</section>
+	<!-- Portfolio section end -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/sections/ServiceSection.vue
@@ -0,0 +1,80 @@
+<template>
+    <!-- Services section start -->
+	<section id="services-section" class="px-12 lg:px-32 py-16 relative overflow-hidden">
+        <!-- Text wrapper start -->
+        <div class="flex gap-8 flex-col md:flex-row">
+            <!-- Main text start -->
+            <h1 class="uppercase text-5xl mb-4 font-semibold ">
+                OUR SERVICES
+            </h1>
+            <p class="capitalize xl:w-1/2 text-stone-500 mb-8">
+                By the use of the qr/sms code, we will generate qr codes for a certain places here in manila to provide informations that the historical markers lack for the tourists to know more about the places, we can also provide qr codes for the certain landmarks that you prefer to have then you can choose where to put the qr codes.
+            </p>
+            <!-- Main text end -->
+        </div>
+        <!-- Text wrapper end -->
+
+        <!-- Services start -->
+        <div class="flex flex-col md:flex-row md:gap-8">
+            <!-- Wedding service start -->
+            <div class="flex flex-row md:flex-col justify-between md:justify-start items-center md:items-start my-8 md:w-1/3">
+                <img src="@/assets/services/wedding.png " alt="Wedding" class="-z-50 md:mb-4">
+                <div class="w-2/3 text-right md:text-left md:w-full">
+                    <h1 class="uppercase text-2xl font-semibold">
+                        Imagination
+                    </h1>
+                    <p class="capitalize text-stone-500">
+                        Does being involve of the events from the past will give you more interesting ideas?
+                    </p>
+                </div>
+            </div>
+            <!-- Wedding service end -->
+
+            <!-- Wedding service start -->
+            <div class="flex flex-row md:flex-col justify-between items-center md:items-start my-8 md:w-1/3">
+                <div class="w-2/3 text-left md:w-full md:order-2">
+                    <h1 class="uppercase text-2xl font-semibold">
+                        Lifestyle
+                    </h1>
+                    <p class="capitalize text-stone-500">
+                        Is the life of the past are better than what we have today?
+                    </p>
+                </div>
+                <img src="@/assets/services/lifestyle.png" alt="Lifestyle" class="-z-50 md:mb-4 md:order-1">
+            </div>
+            <!-- Wedding service end -->
+
+            <!-- Wedding service start -->
+            <div class="flex flex-row md:flex-col justify-between items-center md:items-start my-8 md:w-1/3">
+                <div class="w-1/3">
+                    <img src="@/assets/services/moments.png" alt="Moments" class="-z-50 md:mb-4">
+                </div>
+                <div class="w-2/3 text-right md:text-left md:w-full">
+                    <h1 class="uppercase text-2xl font-semibold">
+                        Moments
+                    </h1>
+                    <p class="capitalize text-stone-500">
+                        What are the intersting events that happened before?
+                    </p>
+                </div>
+            </div>
+            <!-- Wedding service end -->
+        </div>
+        <!-- Services end -->
+
+        <!-- Circle start -->
+        <div class="bg-neutral-300 h-64 w-64 rounded-full absolute top-0 -right-20 mt-16 -z-20"></div>
+        <!-- Circle end -->
+	</section>
+	<!-- Services section end -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
